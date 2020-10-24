7dbc44bb7545d174b669795cefab54b1d33c28b6
Irina Selivanova <irina.selivanova1106@gmail.com>
2020-10-24 22:05:50 +0300

    feat: add weather forecast

--- momentum/script.js
+++ momentum/script.js
@@ -11,27 +11,66 @@ const time = document.getElementById('time'),
     name = document.getElementById('name'),
     focus = document.getElementById('focus'),
     leftArrow = document.getElementById('left-arrow'),
-    rightArrow = document.getElementById('right-arrow');
-let deltaHours = 0,
-    imgSet = generateImageSet();
-
-// Generate Image Set
-function generateImageSet() {
-    let imageSetLocalStorage = parseInt(localStorage.getItem('imgSet')),
-        currentRandomInt = getRandomInt(3);
-
-    if (imageSetLocalStorage === null) {
-        localStorage.setItem('imgSet', 0);
-        return 0;
-    } else {
-        while (imageSetLocalStorage === currentRandomInt) {
-            currentRandomInt = getRandomInt(3);
+    rightArrow = document.getElementById('right-arrow'),
+    weatherIcon = document.querySelector('.weather-icon'),
+    temperature = document.querySelector('.temperature'),
+    weatherDescription = document.querySelector('.weather-description'),
+    blockquote = document.querySelector('blockquote'),
+    figcaption = document.querySelector('figcaption'),
+    changeQuote = document.querySelector('.change-quote'),
+    quotes = {
+        0: {
+            'text': 'Liberty, taking the word in its concrete sense, consists in the ability to choose',
+            'author': 'Simone Weil'
+        },
+        1: {
+            'text': 'Be less curious about people and more curious about ideas',
+            'author': 'Marie Curie'
+        },
+        2: {
+            'text': 'Aim for success, not perfection. Never give up your right to be wrong, because then you will lose the ability to learn new things and move forward with your life',
+            'author': 'Dr. David M. Burns'
+        },
+        3: {
+            'text': 'Myths which are believed in tend to become true',
+            'author': 'George Orwell'
+        },
+        4: {
+            'text': 'Be kind whenever possible. It is always possible',
+            'author': 'Dalai Lama'
+        },
+        5: {
+            'text': 'I am a part of all that I have met',
+            'author': 'Alfred Tennyson'
+        },
+        6: {
+            'text': 'However many holy words you read, however many you speak, what good will they do you if you do not act on upon them?',
+            'author': 'Buddha'
+        },
+        7: {
+            'text': 'In the end we retain from our studies only that which we practically apply',
+            'author': 'Johann Wolfgang von Goethe'
+        },
+        8: {
+            'text': 'Everything is perfect in the universe — even your desire to improve it',
+            'author': 'Wayne Dyer'
+        },
+        9: {
+            'text': 'The pain passes, but the beauty remains',
+            'author': 'Pierre Auguste Renoir'
+        },
+        10: {
+            'text': 'Problems are only opportunities with thorns on them',
+            'author': 'Hugh Miller'
         }
-        localStorage.setItem('imgSet', currentRandomInt);
-    }
+    },
+    city = document.querySelector('.city'),
+    windSpeed = document.querySelector('.wind-speed'),
+    relativeHumidity = document.querySelector('.relative-humidity');
 
-    return currentRandomInt;
-}
+let deltaHours = 0,
+    imgSet = generateImageSet(),
+    currentQuoteId = getRandomInt(10);
 
 // Show Date
 function showDate() {
@@ -122,6 +161,24 @@ function setBgByHours (hour) {
     document.body.style.backgroundImage = `url(${src})`;
 }
 
+// Generate Image Set
+function generateImageSet() {
+    let imageSetLocalStorage = parseInt(localStorage.getItem('imgSet')),
+        currentRandomInt = getRandomInt(3);
+
+    if (imageSetLocalStorage === null) {
+        localStorage.setItem('imgSet', 0);
+        return 0;
+    } else {
+        while (imageSetLocalStorage === currentRandomInt) {
+            currentRandomInt = getRandomInt(3);
+        }
+        localStorage.setItem('imgSet', currentRandomInt);
+    }
+
+    return currentRandomInt;
+}
+
 // Set Greeting by hours
 function setGreetingsByHours (hour) {
     let dayTime = getDayTime(hour);
@@ -208,81 +265,17 @@ function onBlurFocus(e) {
     }
 }
 
+// Change delta for hours
 function changeHoursDelta() {
     deltaHours += parseInt(this.dataset.value);
 }
 
-name.addEventListener('keypress', onKeypressName);
-name.addEventListener('blur', onBlurName);
-name.addEventListener('focus', onFocusName);
-focus.addEventListener('keypress', onKeypressFocus);
-focus.addEventListener('blur', onBlurFocus);
-focus.addEventListener('focus', onFocusName);
-leftArrow.addEventListener('click', changeHoursDelta);
-rightArrow.addEventListener('click', changeHoursDelta);
-
-//Run
-showDate();
-showTime();
-getName();
-getFocus();
-
-const blockquote = document.querySelector('blockquote');
-const figcaption = document.querySelector('figcaption');
-const changeQuote = document.querySelector('.change-quote');
-
+// Get Max random integer
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
 }
 
-const quotes = {
-    0: {
-        'text': 'Liberty, taking the word in its concrete sense, consists in the ability to choose',
-        'author': 'Simone Weil'
-    },
-    1: {
-        'text': 'Be less curious about people and more curious about ideas',
-        'author': 'Marie Curie'
-    },
-    2: {
-        'text': 'Aim for success, not perfection. Never give up your right to be wrong, because then you will lose the ability to learn new things and move forward with your life',
-        'author': 'Dr. David M. Burns'
-    },
-    3: {
-        'text': 'Myths which are believed in tend to become true',
-        'author': 'George Orwell'
-    },
-    4: {
-        'text': 'Be kind whenever possible. It is always possible',
-        'author': 'Dalai Lama'
-    },
-    5: {
-        'text': 'I am a part of all that I have met',
-        'author': 'Alfred Tennyson'
-    },
-    6: {
-        'text': 'However many holy words you read, however many you speak, what good will they do you if you do not act on upon them?',
-        'author': 'Buddha'
-    },
-    7: {
-        'text': 'In the end we retain from our studies only that which we practically apply',
-        'author': 'Johann Wolfgang von Goethe'
-    },
-    8: {
-        'text': 'Everything is perfect in the universe — even your desire to improve it',
-        'author': 'Wayne Dyer'
-    },
-    9: {
-        'text': 'The pain passes, but the beauty remains',
-        'author': 'Pierre Auguste Renoir'
-    },
-    10: {
-        'text': 'Problems are only opportunities with thorns on them',
-        'author': 'Hugh Miller'
-    }
-};
-
-let currentQuoteId = getRandomInt(10);
+// Specify the current quote to the page
 function setQuote() {
     let newQuoteId = currentQuoteId;
 
@@ -295,5 +288,109 @@ function setQuote() {
     blockquote.textContent = quotes[currentQuoteId].text;
     figcaption.textContent = quotes[currentQuoteId].author;
 }
+
+// Show Weather
+async function getWeather(targetCityName) {
+    // Bind function event
+    if (typeof targetCityName === "object") {
+        return ;
+    }
+
+    // Call without function parameter
+    if (typeof targetCityName === "undefined") {
+        targetCityName = getStorageCityName();
+    }
+
+    // Check if it is a number
+    if (/^\d+$/.test(targetCityName)) {
+        notValidWeatherRequest();
+        return ;
+    }
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${targetCityName}&lang=en&appid=289a494ad9bec8e06b1b253fdaf32991&units=metric`,
+        res = await fetch(url),
+        data = await res.json();
+
+    if (data.cod === 200) {
+        localStorage.setItem('city', targetCityName);
+        city.textContent = targetCityName;
+        weatherIcon.className = 'weather-icon owf';
+        weatherIcon.classList.add(`owf-${data.weather[0].id}`);
+        temperature.textContent = `${data.main.temp}°C`;
+        weatherDescription.textContent = data.weather[0].description;
+        windSpeed.textContent = `wind speed ${data.wind.speed} m/s`;
+        relativeHumidity.textContent = `humidity ${data.main.humidity} %`;
+    } else {
+        notValidWeatherRequest();
+    }
+}
+
+// Not valid weather request
+function notValidWeatherRequest()
+{
+    alert("The city is not correct. Please, check the input value.");
+    city.textContent = getStorageCityName();
+}
+
+// Get City Name
+function getStorageCityName() {
+    let cityName;
+    if (localStorage.getItem('city') === null) {
+        localStorage.setItem('city', 'Minsk');
+    }
+    cityName = localStorage.getItem('city');
+
+    return cityName;
+}
+
+// // Change Weather with City
+// function setCity(event) {
+//     if (event.code === 'Enter') {
+//         getWeather();
+//         city.blur();
+//     }
+// }
+
+// On Keypress City
+function onKeypressCity(e) {
+    if (e.type === 'keypress') {
+        // Make sure enter is pressed
+        if (e.which == 13 || e.keyCode == 13) {
+            city.blur();
+        }
+    }
+}
+
+// On Blur City
+function onBlurCity(e) {
+    let inputValue = e.target.innerText;
+
+    if (inputValue.replace(/\s/g, '') !== '') {
+        getWeather(inputValue);
+    } else {
+        city.textContent = localStorage.getItem('city');
+    }
+}
+
+// Listeners
+name.addEventListener('keypress', onKeypressName);
+name.addEventListener('blur', onBlurName);
+name.addEventListener('focus', onFocusName);
+focus.addEventListener('keypress', onKeypressFocus);
+focus.addEventListener('blur', onBlurFocus);
+focus.addEventListener('focus', onFocusName);
+leftArrow.addEventListener('click', changeHoursDelta);
+rightArrow.addEventListener('click', changeHoursDelta);
 document.addEventListener('DOMContentLoaded', setQuote);
 changeQuote.addEventListener('click', setQuote);
+document.addEventListener('DOMContentLoaded', getWeather);
+// city.addEventListener('keypress', setCity);
+city.addEventListener('keypress', onKeypressCity);
+city.addEventListener('blur', onBlurCity);
+
+// Run
+showDate();
+showTime();
+getName();
+getFocus();
+getWeather();
