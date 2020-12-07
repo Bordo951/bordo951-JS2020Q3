73980fa3c2c4a0719e959b1ab0914f9e8ffbc0ca
Irina Selivanova <irina.selivanova1106@gmail.com>
2020-12-08 01:54:18 +0300

    fix: modify finish game

--- english-for-kids/src/scripts/helper/event-manager.js
+++ english-for-kids/src/scripts/helper/event-manager.js
@@ -1,9 +1,11 @@
 import {initRotateCardEvent} from './../events/rotate-card';
 import {initPlayCardSoundEvent} from './../events/play-card-sound';
+import {initClickOnCardPlayModeEvent} from './../events/click-on-card';
 
 export default class EventManager {
     initCardsEvents() {
         initRotateCardEvent();
         initPlayCardSoundEvent();
+        initClickOnCardPlayModeEvent();
     }
 }
